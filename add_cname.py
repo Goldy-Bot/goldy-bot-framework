82edd54f59614fad649dfe09950810b78df28eca
Dev Goldy <jassim7256@gmail.com>
2023-04-07 16:39:02 +0100

    updated add_cname.py script

--- add_cname.py
+++ add_cname.py
@@ -4,4 +4,4 @@ Just a script that is ran by GitHub actions to add the CNAME file to the built d
 
 CNAME = "goldybot.devgoldy.me"
 
-open("./docs/_build/CNAME", mode="w").write(CNAME)
+open("./_build/CNAME", mode="w").write(CNAME)
