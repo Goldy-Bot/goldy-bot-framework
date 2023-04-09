80854bf446e096c35dc61b0f014c8af3dd7e46fc
Dev Goldy <jassim7256@gmail.com>
2023-04-09 02:00:38 +0100

    docs grammar correction

--- GoldyBot/goldy/commands/decorator.py
+++ GoldyBot/goldy/commands/decorator.py
@@ -34,7 +34,7 @@ def command(
 
     .. warning::
 
-        Do note that standalone commands are no longer a thing in goldy bot v5 so you WILL need a register this command inside an Extension. Visit `here`_ to find out how to create extensions.
+        Do note that standalone commands are no longer a thing in goldy bot v5 so you WILL need to register this command inside an Extension. Visit `here`_ to find out how to create extensions.
 
     .. _here: https://goldybot.devgoldy.me/goldy.extensions.html#how-to-create-an-extension
     
