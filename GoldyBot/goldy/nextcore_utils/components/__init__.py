5d7bfa910345c95b8422388b212292935c6e50e3
Dev Goldy <jassim7256@gmail.com>
2023-04-17 16:17:55 +0100

    updates to button class

--- GoldyBot/goldy/nextcore_utils/components/__init__.py
+++ GoldyBot/goldy/nextcore_utils/components/__init__.py
@@ -1,19 +1,30 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, Callable, TYPE_CHECKING, Any, Type
 from discord_typings import ActionRowData, ComponentData
+
+from ... import objects
+
+RECIPE_CALLBACK = Callable[[objects.GoldPlatter], Any]
 
 # TODO: Add functionality to register recipes callback just like a command with a decorator. 
 # (i think a bowl recipe should also be a command object too or at least should contain a command object.)
 
 class BowlRecipe(dict):
     """A bowl recipe is equivalent to an item or message component. This is inherited by all messages components in Goldy Bot. This can be passed into a GoldenBowl() class."""
-    def __init__(self, data: ComponentData) -> ComponentData:
+    def __init__(self, data: ComponentData, callback: RECIPE_CALLBACK = None) -> ComponentData:
         """
         Creates an component in discord to use in action rows. 😋
         
         ⭐ Documentation at https://discord.com/developers/docs/interactions/message-components#action-rows
         """
+        if callback is not None:
+            # TODO: Add to cache.
+            #data["custom_id"]
+            ...
+
+        self.callback = callback
+        """The function to be executed on recipe interaction."""
         
         super().__init__(data)
 
--- GoldyBot/goldy/nextcore_utils/components/buttons/button.py
+++ GoldyBot/goldy/nextcore_utils/components/buttons/button.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import os
 from enum import Enum
 from typing import overload, Literal
 from discord_typings import ButtonComponentData
 
-from .. import BowlRecipe
+from .. import BowlRecipe, RECIPE_CALLBACK
 
 class ButtonStyle(Enum):
     PRIMARY = 1
@@ -18,34 +19,53 @@ class ButtonStyle(Enum):
     GREEN = SUCCESS
     RED = DANGER
 
-# TODO: Create random custom id based on hash of button dict and time it was made.
-random_custom_id = 1 # I know there's better ways to do this dw.
-
 class Button(BowlRecipe):
     """A class used to create a slash command button."""
     @overload
-    def __init__(self, style: ButtonStyle | int, label: str, custom_id: str = None, emoji: str = None, **extra: ButtonComponentData) -> ButtonComponentData:
+    def __init__(
+        self, 
+        style: ButtonStyle | int, 
+        label: str, 
+        callback: RECIPE_CALLBACK, 
+        emoji: str = None, 
+        custom_id: str = None, 
+        **extra: ButtonComponentData
+    ) -> ButtonComponentData:
         ...
 
     @overload
-    def __init__(self, style: Literal[5], label: str, url: str, emoji: str = None, **extra: ButtonComponentData) -> ButtonComponentData:
+    def __init__(
+        self, 
+        style: Literal[5], 
+        label: str, 
+        url: str, 
+        emoji: str = None, 
+        **extra: ButtonComponentData
+    ) -> ButtonComponentData:
         ...
 
-    def __init__(self, style: ButtonStyle | int, label: str, custom_id: str = None, url: str = None, emoji: str = None, **extra: ButtonComponentData) -> ButtonComponentData:
+    def __init__(
+        self, 
+        style: ButtonStyle | int, 
+        label: str, 
+        custom_id: str = None, 
+        url: str = None, 
+        emoji: str = None, 
+        callback: RECIPE_CALLBACK = None, 
+        **extra: ButtonComponentData
+    ) -> ButtonComponentData:
         """
         Creates a discord button to use in action rows. 😋
         
         ⭐ Documentation at https://discord.com/developers/docs/interactions/message-components#buttons
         """
-        global random_custom_id
         data: ButtonComponentData = {}
 
         if isinstance(style, ButtonStyle):
             style = style.value
 
         if custom_id is None:
-            random_custom_id += 1
-            custom_id = str(random_custom_id)
+            custom_id = os.urandom(16).hex()
 
         data["type"] = 2 # ID type for button.
         data["style"] = style
@@ -66,4 +86,4 @@ class Button(BowlRecipe):
 
         data.update(extra)
 
-        super().__init__(data)
+        super().__init__(data, callback)
