6837006e46911fe3e1c059fef993de450ee7c885
Dev Goldy <jassim7256@gmail.com>
2023-04-25 23:57:34 +0100

    fixed ruff error

--- GoldyBot/goldy/nextcore_utils/embeds/embed.py
+++ GoldyBot/goldy/nextcore_utils/embeds/embed.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, overload
+from typing import List
 from discord_typings import EmbedData, EmbedFieldData, EmbedImageData
 from ..colours import Colours
 
