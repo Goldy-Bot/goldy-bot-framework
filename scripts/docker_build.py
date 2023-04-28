a4519f971d18dc5d9a5428ff5d947304377929ff
Dev Goldy <jassim7256@gmail.com>
2023-04-28 19:34:41 +0100

    added architecture flag

--- scripts/docker_build.py
+++ scripts/docker_build.py
@@ -4,9 +4,9 @@ import os
 from GoldyBot.info import VERSION
 
 os.system(
-    f"docker build -t devgoldy/goldybot:{VERSION} ."
+    f"docker build -t devgoldy/goldybot:{VERSION} --build-arg ARCH=amd64 ."
 )
 
 os.system(
-    "docker build -t devgoldy/goldybot:latest ."
+    "docker build -t devgoldy/goldybot:latest --build-arg ARCH=amd64 ."
 )
