5b0f4bb7eda29cb5d9b7babc8c89a0b49c622a3f
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-05-03 17:45:44 +0100

    Revert "fixed bugs in strings.line_fix() and make goldy command bot dev only"
    This reverts commit 00d5cb3ebc6e633663e287bccd0b8a64e00b2789.

--- GoldyBot/goldy/utils/strings.py
+++ GoldyBot/goldy/utils/strings.py
@@ -2,6 +2,4 @@ import re
 
 def line_fix(string: str) -> str:
     """Fixes this -> https://github.com/Goldy-Bot/Goldy-Bot-V5/issues/37"""
-    return "".join(
-        [re.sub(' +', ' ', line)[1 if string[0] == "\n" else 0:] + "\n" for line in string.splitlines()]
-    )
+    return "".join([re.sub(' +', ' ', line)[1:] + "\n" for line in string.splitlines()])
