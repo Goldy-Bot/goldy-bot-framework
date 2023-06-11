528eda81fe8d1131373b5730d6e7218561449481
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-06-11 22:59:23 +0100

    Revert "updated timestamps extension"
    This reverts commit 45d18df089764233ae58009a875a3018aab815a6.

--- GoldyBot/internal_extensions/extensions.py
+++
@@ -1,9 +0,0 @@
-import GoldyBot
-
-class Extensions(GoldyBot.Extension):
-    def __init__(self):
-        super().__init__()
-
-    @GoldyBot.command()
-    async def enable(self, platter: GoldyBot.GoldPlatter, extension: str):
-        ...
