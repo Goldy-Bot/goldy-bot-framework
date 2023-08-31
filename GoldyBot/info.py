45dd316a2b289670bde0990f677a385cfb4f2d4d
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-09-01 00:41:11 +0100

    version bump

--- GoldyBot/info.py
+++ GoldyBot/info.py
@@ -2,7 +2,7 @@
 # --------------
 VER = 5.0
 """Just the version number as an integer. E.g ``5.0``."""
-STAGE = ("dev", 9)
+STAGE = ("dev", 10)
 
 VERSION = f"{VER}{STAGE[0]}{STAGE[1]}"
 """Goldy Bot version string. E.g ``5.0alpha8``."""
