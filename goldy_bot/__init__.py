0a92776effa1cd783a0a913315121f85ffa9ffef
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-12-22 18:18:37 +0000

    chore: started populating pancake

---
+++ goldy_bot/__init__.py
@@ -0,0 +1,7 @@
+"""
+💛 Goldy Bot V5.1 - 🥞 Pancake
+
+Copyright (C) 2023 - 2024 Goldy
+"""
+
+from .goldy import *
---
+++ goldy_bot/goldy.py
@@ -0,0 +1,8 @@
+
+__all__ = (
+    "Goldy",
+)
+
+class Goldy():
+    """The class containing all the framework's core components; pretty much the core of Goldy Bot."""
+    ...
